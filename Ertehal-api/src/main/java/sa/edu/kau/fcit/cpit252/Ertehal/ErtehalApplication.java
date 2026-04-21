5af944664b1e181dc559aa3543368d4dc92de4e2
Ali Judah <ali.judaj.kau@gmail.com>
2026-04-21 20:35:02 +0300

    finishing the the setup of the project #1

---
+++ Ertehal-api/src/main/java/sa/edu/kau/fcit/cpit252/Ertehal/ErtehalApplication.java
@@ -0,0 +1,13 @@
+package sa.edu.kau.fcit.cpit252.Ertehal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErtehalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErtehalApplication.class, args);
+	}
+
+}
---
+++ Ertehal-api/src/test/java/sa/edu/kau/fcit/cpit252/Ertehal/ErtehalApplicationTests.java
@@ -0,0 +1,13 @@
+package sa.edu.kau.fcit.cpit252.Ertehal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErtehalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
